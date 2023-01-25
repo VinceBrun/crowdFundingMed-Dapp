5ddf981d952c50bfd48223f54d8b6995c79c1313
VinceBruno <ambavincent1203@gmail.com>
2023-01-25 14:07:16 +0100

    add

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,10 +14,10 @@ const Navbar = () => {
 
   return (
     <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
-      <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-gradient-to-r from-red-600 to-orange-300 rounded-[100px]">
+      <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
         <input type="text" placeholder="Search for campaigns" className="flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none" />
         
-        <div className="w-[72px] h-full rounded-[20px] bg-[#fdba74] flex justify-center items-center cursor-pointer">
+        <div className="w-[72px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center items-center cursor-pointer">
           <img src={search} alt="search" className="w-[15px] h-[15px] object-contain"/>
         </div>
       </div>
@@ -26,7 +26,7 @@ const Navbar = () => {
         <CustomButton 
           btnType="button"
           title={address ? 'Create a campaign' : 'Connect'}
-          styles={address ? 'bg-[#dc2626]' : 'bg-[#fdba74]'}
+          styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
           handleClick={() => {
             if(address) navigate('create-campaign')
             else connect()
@@ -34,7 +34,7 @@ const Navbar = () => {
         />
 
         <Link to="/profile">
-          <div className="w-[52px] h-[52px] rounded-full bg-gradient-to-r from-red-600 to-orange-300 flex justify-center items-center cursor-pointer">
+          <div className="w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer">
             <img src={thirdweb} alt="user" className="w-[60%] h-[60%] object-contain" />
           </div>
         </Link>
@@ -42,7 +42,7 @@ const Navbar = () => {
 
       {/* Small screen navigation */}
         <div className="sm:hidden flex justify-between items-center relative">
-        <div className="w-[40px] h-[40px] rounded-[10px] bg-gradient-to-r from-red-600 to-orange-300 flex justify-center items-center cursor-pointer">
+        <div className="w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer">
             <img src={logo} alt="user" className="w-[60%] h-[60%] object-contain" />
           </div>
 
@@ -53,12 +53,12 @@ const Navbar = () => {
             onClick={() => setToggleDrawer((prev) => !prev)}
           />
 
-          <div className={`absolute top-[60px] right-0 left-0 bg-[#fdba74] z-10 shadow-secondary py-4 ${!toggleDrawer ? '-translate-y-[100vh]' : 'translate-y-0'} transition-all duration-700`}>
+          <div className={`absolute top-[60px] right-0 left-0 bg-[#1c1c24] z-10 shadow-secondary py-4 ${!toggleDrawer ? '-translate-y-[100vh]' : 'translate-y-0'} transition-all duration-700`}>
             <ul className="mb-4">
               {navlinks.map((link) => (
                 <li
                   key={link.name}
-                  className={`flex p-4 ${isActive === link.name && 'bg-[#dc2626]'}`}
+                  className={`flex p-4 ${isActive === link.name && 'bg-[#3a3a43]'}`}
                   onClick={() => {
                     setIsActive(link.name);
                     setToggleDrawer(false);
@@ -81,7 +81,7 @@ const Navbar = () => {
             <CustomButton 
               btnType="button"
               title={address ? 'Create a campaign' : 'Connect'}
-              styles={address ? 'bg-[#dc2626]' : 'bg-[#fdba74]'}
+              styles={address ? 'bg-[#1dc071]' : 'bg-[#8c6dfd]'}
               handleClick={() => {
                 if(address) navigate('create-campaign')
                 else connect();
