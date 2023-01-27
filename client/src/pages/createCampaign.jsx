80e9f66161aff3cb2e193c74e2fe03358c46eba6
VinceBruno <ambavincent1203@gmail.com>
2023-01-27 14:48:39 +0100

    add

--- client/src/pages/createCampaign.jsx
+++ client/src/pages/createCampaign.jsx
@@ -41,10 +41,10 @@ const CreateCampaign = () => {
   }
 
   return (
-    <div className="bg-gradient-to-r from-red-600 to-orange-300 flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
+    <div className="bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
       {isLoading && <Loader />}
-      <div className="flex justify-center items-center p-[16px] sm:min-w-[380px] bg-gradient-to-r from-red-600 to-orange-300 rounded-[10px]">
-        <h1 className="font-epilogue font-bold sm:text-[25px] text-[18px] leading-[38px] text-white">Begin a Campaign</h1>
+      <div className="flex justify-center items-center p-[16px] sm:min-w-[380px] bg-[#3a3a43] rounded-[10px]">
+        <h1 className="font-epilogue font-bold sm:text-[25px] text-[18px] leading-[38px] text-white">Start a Campaign</h1>
       </div>
 
       <form onSubmit={handleSubmit} className="w-full mt-[65px] flex flex-col gap-[30px]">
@@ -73,9 +73,9 @@ const CreateCampaign = () => {
             handleChange={(e) => handleFormFieldChange('description', e)}
           />
 
-        <div className="w-full flex justify-start items-center p-4 bg-gradient-to-r from-red-600 to-orange-300 h-[120px] rounded-[10px]">
+        <div className="w-full flex justify-start items-center p-4 bg-[#8c6dfd] h-[120px] rounded-[10px]">
           <img src={money} alt="money" className="w-[40px] h-[40px] object-contain"/>
-          <h4 className="font-epilogue font-bold text-[25px] text-white ml-[20px]">Gain 100% of the raised amount</h4>
+          <h4 className="font-epilogue font-bold text-[25px] text-white ml-[20px]">You will get 100% of the raised amount</h4>
         </div>
 
         <div className="flex flex-wrap gap-[40px]">
@@ -97,8 +97,8 @@ const CreateCampaign = () => {
 
         <FormField 
             labelName="Campaign image *"
-            placeholder="Place image of your campaign"
-            inputType="image"
+            placeholder="Place image URL of your campaign"
+            inputType="url"
             value={form.image}
             handleChange={(e) => handleFormFieldChange('image', e)}
           />
@@ -107,7 +107,7 @@ const CreateCampaign = () => {
             <CustomButton 
               btnType="submit"
               title="Submit new campaign"
-              styles="bg-[#fdba74]"
+              styles="bg-[#1dc071]"
             />
           </div>
       </form>
