ac86b7fc1572f2b9a054452f11072d1367fc6fe6
VinceBruno <ambavincent1203@gmail.com>
2023-01-27 14:57:14 +0100

    add

--- client/src/pages/campaignDetails.jsx
+++ client/src/pages/campaignDetails.jsx
@@ -115,7 +115,7 @@ const CampaignDetails = () => {
               />
 
               <div className="my-[20px] p-4 bg-[#13131a] rounded-[10px]">
-                <h4 className="font-epilogue font-semibold text-[14px] leading-[22px] text-white">Back it because you believe in it.</h4>
+                <h4 className="font-epilogue font-semibold text-[14px] leading-[22px] text-white">Back it for the sake of love and charity (charity covers a multitude of sin).</h4>
                 <p className="mt-[20px] font-epilogue font-normal leading-[22px] text-[#808191]">Support the project for no reward, just because it speaks to you.</p>
               </div>
 
